a84c9e65e87212639663508561333e134bec649d
Drihan du Preez <drihan@btinternet.com>
2026-01-06 15:58:01 +0000

    sql sequence used to create table

---
+++ Table_Creation.sql
@@ -0,0 +1,33 @@
+CREATE TABLE engine_readings (
+    engine_id INTEGER,
+    cycle INTEGER,
+
+    op_setting_1 FLOAT,
+    op_setting_2 FLOAT,
+    op_setting_3 FLOAT,
+
+    sensor_1 FLOAT,
+    sensor_2 FLOAT,
+    sensor_3 FLOAT,
+    sensor_4 FLOAT,
+    sensor_5 FLOAT,
+    sensor_6 FLOAT,
+    sensor_7 FLOAT,
+    sensor_8 FLOAT,
+    sensor_9 FLOAT,
+    sensor_10 FLOAT,
+    sensor_11 FLOAT,
+    sensor_12 FLOAT,
+    sensor_13 FLOAT,
+    sensor_14 FLOAT,
+    sensor_15 FLOAT,
+    sensor_16 FLOAT,
+    sensor_17 FLOAT,
+    sensor_18 FLOAT,
+    sensor_19 FLOAT,
+    sensor_20 FLOAT,
+    sensor_21 FLOAT,
+
+    dataset_id TEXT,
+    data_split TEXT
+);
